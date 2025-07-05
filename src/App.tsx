a8a7ce4b70201ad71694c536fe5001d2964d6b22
bb-hiroto-ishii <hiroto.ishii@bondband.jp>
2025-07-06 07:26:23 +0900

    brush up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import StatusBar from "./StatusBar";
 
 const images = import.meta.glob('/src/assets/monsters/*.jpg', { eager: true }) as Record<string, { default: string }>;
 
-const icons = Object.values(images).map((mod) => mod.default);
+const allIcons = Object.values(images).map((mod) => mod.default);
 
-const createShuffledCards = () => {
-  const cardTypes = icons.sort(() => Math.random() - 0.5).slice(0, 8); // 6種類のカードをランダムに選出
-  const shuffled = [...cardTypes, ...cardTypes].sort(() => Math.random() - 0.5);
+interface Card {
+  id: number,
+  icon: string,
+  flipped: boolean,
+  matched: boolean,
+}
+
+const createShuffledCards = (): Card[] => {
+  const icons = allIcons.sort(() => Math.random() - 0.5).slice(0, 8); // 8種類のカードをランダムに選出
+  const shuffled = [...icons, ...icons].sort(() => Math.random() - 0.5);
   return shuffled.map((icon, index) => ({
     id: index,
     icon,
@@ -19,15 +26,23 @@ const createShuffledCards = () => {
 
 function App() {
   useEffect(() => {
-    resetGame(); // 初期化
+    allIcons.forEach((src) => {
+      const img = new window.Image();
+      img.src = src;
+    });
   }, []);
-
-  const [cards, setCards] = useState(createShuffledCards());
+  
+  const shuffledCardsRef = useRef(createShuffledCards());
+  const [cards, setCards] = useState(shuffledCardsRef.current);
   const [selected, setSelected] = useState<number[]>([]);
   const [point, setPoint] = useState(0);
   const [comboCount, setComboCount] = useState(0);
   const [isResetting, setIsResetting] = useState(false);
   const [countdown, setCountdown] = useState<number | string | null>(null);
+
+  useEffect(() => {
+    setCards(shuffledCardsRef.current);
+  }, []);
 
   const flipCard = (index: number) => {
     if (isResetting) return; // リセット中は何もしない
@@ -67,38 +82,57 @@ function App() {
   };
 
   const resetGame = () => {
+    if (isResetting) return; // リセット中は何もしない
     setIsResetting(true);
+    resetParameter();
+
+    shuffledCardsRef.current = createShuffledCards();
+
+    const hiddenCards = shuffledCardsRef.current.map(card => {
+      if(!card.flipped) return card; 
+      return {
+        ...card,
+        flipped: false
+      };
+    });
+    setCards(hiddenCards);
 
     let counter = 3;
     const interval = setInterval(() => {
       if (counter === 0) {
         clearInterval(interval);
         setCountdown(null);
+        showCards();
 
-        const newCards = createShuffledCards().map((card) => ({
-          ...card,
-          flipped: true, // 一旦すべて表向きに
-        }));
-
-        setCards(newCards);
-        setSelected([]);
-        setPoint(0);
-        setComboCount(0);
-
-        setTimeout(() => {
-          const hiddenCards = newCards.map((card) => ({
-            ...card,
-            flipped: false,
-          }));
-          setCards(hiddenCards);
-          setIsResetting(false);
-        }, 5000); // 5秒（5000ミリ秒）
       } else {
         setCountdown(counter);
-  }
+      }
       counter--;
     }, 1000);
   };
+
+  const resetParameter = () => {
+    setSelected([]);
+    setPoint(0);
+    setComboCount(0);
+  }
+
+  const showCards = () => {
+    const newCards = shuffledCardsRef.current.map((card) => ({
+      ...card,
+      flipped: true, // 一旦すべて表向きに
+    }));
+
+    setCards(newCards);
+    setTimeout(() => {
+      const hiddenCards = shuffledCardsRef.current.map((card) => ({
+        ...card,
+        flipped: false,
+      }));
+      setCards(hiddenCards);
+      setIsResetting(false);
+    }, 5000); // 5秒（5000ミリ秒）
+  }
 
   return (
     <div className="container">
@@ -111,20 +145,23 @@ function App() {
       <h1>Match Monster</h1>
     </header>    
       <StatusBar point={point} comboCount={comboCount} />
-    <div className="grid">
-      {cards.map((card, i) => (
-        <button
-          key={card.id}
-          onClick={() => flipCard(i)}
-          disabled={card.matched}
-          className={`card ${!card.flipped && !card.matched ? 'back' : 'front'}`}
-        >
-          {card.flipped || card.matched ? (
-            <img src={card.icon} alt="monster" className='card-image'/>
-          ) : null}
-        </button>
-      ))}
-    </div>
+      <div className="grid">
+        {cards.map((card, i) => (
+          <button
+            key={card.id}
+            onClick={() => flipCard(i)}
+            disabled={card.flipped || card.matched}
+            className="card"
+          >
+            <img src={card.icon} alt="monster" className="card-image" />
+            <img
+              src="/card-back.png"
+              alt="back"
+              className={`card-back-image ${card.flipped ? "hidden" : ""}`}
+            />
+          </button>
+        ))}
+      </div>
       <footer className="footer">
         <button className="reset-button" onClick={resetGame}>
           🔁 
